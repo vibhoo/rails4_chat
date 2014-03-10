7b0887921c7588cb48b5bb911305567c1d88b7ba
vibhoo <mishrarock2010@gmail.com>
2014-03-10 17:25:48 +0530

    pushing to gihub

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Practice::Application.config.secret_key_base = '92de3a06dd48d054a9ba4773756eac9a45822565771afb093c85df91bfb0ba57ea1f789f77ae52922cf71c35013810be64a932fb7c1a848d6c54e42029d48ca1'
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+Practice::Application.routes.draw do
+  get "public/index"
+  root to: "public#index"
+end
